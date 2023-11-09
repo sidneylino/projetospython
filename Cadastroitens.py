0a3778937725e0aad2d4f322a8dd2c4655711b75
sidneylino <sidneymartinslino@gmail.com>
2023-11-08 23:22:14 -0300

    versao 1.3

--- Cadastroitens.py
+++ Cadastroitens.py
@@ -1,4 +1,16 @@
-print("olar mundo")
-a=int(input("digite u numero"))
-print(a)
-print("ata\n ata")
+menu = int(input("selecione a opção:\n[1]cadastrar produtos\n[2]buscar produtos\n[3]encerrar"))
+if menu == 1:
+    x=1
+    produtos = []
+    while (x == 1):
+        nome = input("nome:")
+        quantidade = input("quantos?")
+        prod = [nome,quantidade]
+        produtos.append(prod)
+    x = input("continuar cadastrando tecle 1, fi")
+print("quantidade de prdutos", len(produtos))
+for n in produtos:
+    nome = n[0]
+    quantidade = n[1]
+    print("o produto é", nome,"tem a quantidade:", quantidade)
+
