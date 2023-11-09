22ab35cdc3d07b86325873d11223804a8e8db13a
sidneylino <sidneymartinslino@gmail.com>
2023-11-09 13:29:24 -0300

    versao 1.3

--- Cadastroitens.py
+++ Cadastroitens.py
@@ -1,14 +1,15 @@
-menu = int(input("selecione a opção:\n[1]cadastrar produtos\n[2]buscar produtos\n[3]encerrar"))
-if menu == 1:
+opcao = int(input("selecione a opção:\n[1]cadastrar produtos\n[2]buscar produtos\n[3]encerrar"))
+if opcao == 1:
     x=1
-    produtos = []
-    while (x == 1):
+    catalogo = []
+    while x == 1:
         nome = input("nome:")
         quantidade = input("quantos?")
-        prod = [nome,quantidade]
-        produtos.append(prod)
-    x = input("continuar cadastrando tecle 1, fi")
-print("quantidade de prdutos", len(produtos))
+        produto = [nome,quantidade]
+        catalogo.append(produto)
+        x = int(input("[1] Cadastrar novo produto \n[2]buscar produtos\n [3]Encerrar"))
+
+print("quantidade de produtos:", len(produtos))
 for n in produtos:
     nome = n[0]
     quantidade = n[1]
