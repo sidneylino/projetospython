f443d9140bb8518598052de28bb04fe05699cf68
sidneylino <sidneymartinslino@gmail.com>
2023-11-09 15:29:52 -0300

    versao 1.4

--- Cadastroitens.py
+++ Cadastroitens.py
@@ -1,17 +1,29 @@
-opcao = int(input("selecione a opção:\n[1]cadastrar produtos\n[2]buscar produtos\n[3]encerrar"))
-if opcao == 1:
-    x=1
-    catalogo = []
-    while x == 1:
-        nome = input("nome:")
-        quantidade = input("quantos?")
-        produto = [nome,quantidade]
-        catalogo.append(produto)
-        x = int(input("[1] Cadastrar novo produto \n[2]buscar produtos\n [3]Encerrar"))
+#algoritmo para cadastramento de produtos
+#criado por Sidney Lino
 
-print("quantidade de produtos:", len(produtos))
-for n in produtos:
-    nome = n[0]
-    quantidade = n[1]
-    print("o produto é", nome,"tem a quantidade:", quantidade)
+opcao = int(input("selecione a opção:\n[1]cadastrar produtos\n[2]buscar produtos\n[3]encerrar"))
+catalogo = []
+while opcao == 1:
+    nome = input("nome:")
+    quantidade = input("quantos?")
+    produto = [nome,quantidade]
+    catalogo.append(produto)
+    opcao = int(input("[1] Cadastrar novo produto \n[2]buscar produtos\n [3]Encerrar"))
+    if opcao == 2:
+        print("quantidade de produtos:", len(catalogo))
+        for n in catalogo:
+            nome = n[0]
+            quantidade = n[1]
+            print("produto:", nome, "quantidade:", quantidade)
+    elif opcao == 3:
+        print("programa encerrado")
+    else:
+        opcao = int(input("Opção invalida, digite novamente:"))
+
+
+
+
+
+
+
 
